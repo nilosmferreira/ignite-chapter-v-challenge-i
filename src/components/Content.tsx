805271ed9c65a853e90b2317a5ead56f092476d5
Nilo Ferreira <nsmf.gnf@gmail.com>
2022-01-11 22:28:47 -0300

    terminando o desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,60 @@
-export function Content() {
-  // Complete aqui
+import { memo } from 'react';
+import { Header } from './Header';
+import { MovieCard } from './MovieCard';
+import { ListRowRenderer } from 'react-virtualized';
+
+interface ContentProps {
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>;
+  selectedGenre: {
+    title: string;
+  };
 }
+
+function ContentComponent({ movies, selectedGenre }: ContentProps) {
+  const rowRenderer: ListRowRenderer = ({ index, style, key }) => {
+    const movie = movies[index];
+    console.log(movie);
+    return (
+      <div style={style} key={key}>
+        <MovieCard
+          key={movie.imdbID}
+          title={movie.Title}
+          poster={movie.Poster}
+          runtime={movie.Runtime}
+          rating={movie.Ratings[0].Value}
+        />
+      </div>
+    );
+  };
+  return (
+    <div className="container">
+      <Header selectedGenre={selectedGenre} />
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export const Content = memo(ContentComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps, nextProps);
+});
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { memo } from 'react';
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>;
+  selectedGenreId: number;
+  onClick: (id: number) => void;
 }
+ function SideBarComponent({ genres, selectedGenreId, onClick }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
+
+export const SideBar = memo(SideBarComponent, (prevProps, nextProps)=>{
+  return Object.is(prevProps, nextProps)
+})
